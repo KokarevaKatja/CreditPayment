44ae5cfbed775414f2eeb2e779c87f700bbc9874
Екатерина Кокарева <kokarevakatja@gmail.com>
2022-07-26 19:12:58 +0300

    Final update

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,14 @@
 public class CreditPaymentService {
 
-    public double calculate(int creditAmount, double monthRate, int months) {
+    public double calculate(int creditAmount, double creditRate, int months) {
+
+        double monthRate = creditRate / 12 / 100;
 
         double sum = 1 + monthRate;
 
-        return creditAmount * ((monthRate * Math.pow(sum, months)) / (Math.pow(sum, months) - 1));
+        double creditPayment = creditAmount * ((monthRate * Math.pow(sum, months)) / (Math.pow(sum, months) - 1));
+
+        return creditPayment;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import static java.lang.Math.round;
+
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        System.out.println(service.calculate(1_000_000, 0.8325, 12));
-        System.out.println(service.calculate(1_000_000, 0.4163, 24));
-        System.out.println(service.calculate(1_000_000, 0.2775, 36));
+        int creditAmount = 1_000_000;
+
+        double creditRate = 9.9;
+
+        int months = 12;
+
+        double creditPayment = service.calculate(creditAmount, creditRate, months);
+
+        System.out.println(service.calculate(1_000_000, 9.9, 12));
+        System.out.println(service.calculate(1_000_000, 9.9, 24));
+        System.out.println(service.calculate(1_000_000, 9.9, 36));
 
     }
 }
